01607835d36624adffcc066d77f9dc6d0b5418ee
Nikos Vasilakopoulos <Nickvasilakopoulos@rocketmail.com>
2026-05-01 11:25:03 +0300

    Add 01_above_average solution

---
+++ easy/01_above_average.py
@@ -0,0 +1,16 @@
+#Given a list of numbers prin only the numbers that are greater than the average of the list
+
+
+numbers = [4,8,15,16,23,42]
+
+
+#prepei na brw to average --> uparxei hdh to avg(numbers)
+#loop gia print ari8mwn > average
+
+
+avg = sum(numbers)/len(numbers)
+
+
+for number in numbers:
+    if number > avg:
+        print(number)
